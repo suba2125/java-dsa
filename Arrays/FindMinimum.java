9bde193dd5e563c8392aed19947cc815f17bf7aa
suba2125 <subhasrip212007@gmail.com>
2026-08-07 20:13:06 +0530

    Add FindMinimum class to find minimum in array

---
+++ Arrays/FindMinimum.java
@@ -0,0 +1,16 @@
+public class FindMinimum {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 40, 15};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Minimum: " + min);
+    }
+}
