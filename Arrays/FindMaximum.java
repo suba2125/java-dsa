aa7b472158a9c6f23ba1a4408749e618e4c7b299
suba2125 <subhasrip212007@gmail.com>
2026-08-07 20:08:57 +0530

    Add FindMaximum class to find max in an array

---
+++ Arrays/FindMaximum.java
@@ -0,0 +1,16 @@
+public class FindMaximum {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 40, 15};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+    }
+}
