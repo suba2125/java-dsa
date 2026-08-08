b7375041def0e8fe7a9bfd690d0a1c7c1e81eafe
suba2125 <subhasrip212007@gmail.com>
2026-08-08 20:21:34 +0530

    Add StringBasics class to demonstrate string methods

---
+++ Strings/StringBasics.java
@@ -0,0 +1,11 @@
+public class StringBasics {
+    public static void main(String[] args) {
+
+        String name = "Subhasri";
+
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(3));
+    }
+}
