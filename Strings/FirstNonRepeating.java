8bd9001462ff008b6c3abde87d618adf06f30245
suba2125 <subhasrip212007@gmail.com>
2026-08-11 19:58:08 +0530

    Add FirstNonRepeating class to find unique character

---
+++ Strings/FirstNonRepeating.java
@@ -0,0 +1,24 @@
+public class FirstNonRepeating {
+    public static void main(String[] args) {
+
+        String str = "swiss";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(j) == ch) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("First non-repeating character: " + ch);
+                break;
+            }
+        }
+    }
+}
